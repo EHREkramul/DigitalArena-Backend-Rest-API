4d66d34b5f64e3928993c5614252d2f924c58164
EHREkramul <ehr.ekramul@gmail.com>
2024-12-06 20:03:59 +0600

    Add Reviews module with controller and service

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,9 +6,10 @@ import { UsersModule } from './users/users.module';
 import { ProductsModule } from './products/products.module';
 import { OrdersModule } from './orders/orders.module';
 import { CartModule } from './cart/cart.module';
+import { ReviewsModule } from './reviews/reviews.module';
 
 @Module({
-  imports: [AuthModule, UsersModule, ProductsModule, OrdersModule, CartModule],
+  imports: [AuthModule, UsersModule, ProductsModule, OrdersModule, CartModule, ReviewsModule],
   controllers: [AppController],
   providers: [AppService],
 })
